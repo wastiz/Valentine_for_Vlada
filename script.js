ae11b22f27e8b1a5bd5b95e199c1fe2eb085ab03
Valeri Nossov <valnos04@gmail.com>
2024-02-12 17:04:01 +0200

    another

--- script.js
+++ script.js
@@ -1,4 +1,13 @@
 const noBtn = document.querySelector('.no-button');
+const yesBtn = document.querySelector('.yes-button');
+const nextBtn = document.querySelector('.next-button');
+const firstPart = document.querySelector('.first-part');
+const secondPart = document.querySelector('.second-part');
+const thirdPart = document.querySelector('.third-part');
+const fourhPart = document.querySelector('.fourh-part');
+const loveLetter = document.querySelector('.love-letter');
+
+
 
 function getRandom(min, max) {
     return Math.random() * (max - min) + min;
@@ -13,4 +22,19 @@ noBtn.addEventListener('touchstart', function(e){
     let x = getRandom(-30, 30);
     let y = getRandom(-30, 30);
     e.target.style.cssText = `top: ${y}vh; left: ${x}vh`;
+});
+
+yesBtn.addEventListener('click', function(e){
+    firstPart.classList.add('hidden');
+    secondPart.classList.remove('hidden');
+});
+
+nextBtn.addEventListener('click', function(e){
+    secondPart.classList.add('hidden');
+    thirdPart.classList.remove('hidden');
+});
+
+loveLetter.addEventListener('click', function(e){
+    thirdPart.classList.add('hidden');
+    fourhPart.classList.remove('hidden');
 });
